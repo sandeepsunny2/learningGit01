9bdd741f97cf087e53a7d9cb3929f0a56f07d529
sandeep <sandeep@Sunny>
2018-09-16 16:15:36 +0530

    First Commit - comment java class

---
+++ LearnGit01/src/commentbox/Comments.java
@@ -0,0 +1,12 @@
+package commentbox;
+
+public class Comments {
+
+	// Learning Git - First commit		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, I am new to Git");
+	}
+
+}
